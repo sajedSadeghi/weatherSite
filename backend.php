a50ddb7a4bf1f266d14b9df1df6a82fde85ba452
sajedSadeghi <107381765+sajedSadeghi@users.noreply.github.com>
2023-04-19 00:40:54 +0430

    change ui after getting response update error handlers in main.js and backend.php file fix search button icon

--- backend.php
+++ backend.php
@@ -1,5 +1,4 @@
 <?php
-
 require __DIR__ . '/vendor/autoload.php'; // Path to Composer autoloader
 
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
@@ -17,12 +16,37 @@ try {
     $response = $client->get("https://api.weatherapi.com/v1/current.json?key={$apiKey}&q={$city}&aqi=no", [
         "headers" => $headers
     ]);
-    
+
+    // Check the response status code
+    if ($response->getStatusCode() === 404) {
+        throw new Exception("City not found");
+    }
+
     $data = json_encode(json_decode($response->getBody()), JSON_PRETTY_PRINT);
-    
+
     header('Content-Type: application/json');
+
     echo $data;
+}
+// Display an error message to the user
+
+catch (GuzzleHttp\Exception\ConnectException $e) {
+
+    echo "Sorry, we are unable to connect to the weather API at the moment. Please try again later.";
+
 } catch (GuzzleHttp\Exception\RequestException $e) {
-    // Handle any errors that occur during the API call
-    echo $e->getMessage();
+
+    echo "Sorry, we are unable to retrieve the weather data at the moment. Please try again later.";
+
+} catch (Exception $e) {
+
+    // Check the error message
+    if ($e->getMessage() === "City not found") {
+
+        echo "Sorry, the city you entered could not be found. Please try again with a different city.";
+    } else {
+
+        echo "Sorry, there was an error processing your request. Please try again later.";
+    }
+    
 }
--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const nameOutput = document.querySelector(".name");
 const icon = document.querySelector(".icon");
 const cloudOutput = document.querySelector(".cloud");
 const HumidityOutput = document.querySelector(".Humidity");
-const windOutput = document.querySelector(".wind");
+const windOutput = document.querySelector(".Wind");
 const form = document.getElementById("locationInput");
 const search = document.querySelector(".search");
 const btn = document.querySelector(".submit");
@@ -18,18 +18,145 @@ const cities = document.querySelectorAll(".city");
 let cityInput = "London";
 
 // Send City Name and Get API Response Function
+function fetchWeatherData(cityname) {
+  let loop = true;
 
-function fetchweatherdata(cityname) {
-  $.post(
-    "./backend.php",
-    {city : cityname},
-    function(data) {
-        console.log(data);
+  function handleResponse(data) {
+    // add temperature to page
+    temp.innerHTML = data.current.temp_c + "&#176;";
+    conditionOutput.innerHTML = data.current.condition.text;
 
+    // get date and time from city
+    const date = data.location.localtime;
+    const y = parseInt(date.substr(0, 4));
+    const m = parseInt(date.substr(5, 2));
+    const d = parseInt(date.substr(8, 2));
+    const time = date.substr(11);
+
+    // add date to page
+    dateOutput.innerHTML = `${dayOfTheWeek(d, m, y)} ${d}, ${m} ${y}`;
+    timeOutput.innerHTML = time;
+
+    // add name of the city to page
+    nameOutput.innerHTML = data.location.name;
+
+    // get corresponding icon for weather
+    const iconId = data.current.condition.icon.substr(
+      "//cdn.weatherapi.com/weather/64x64/".length
+    );
+
+    // reformat icon url and add to page
+    icon.src = "./icons/" + iconId;
+
+    // add weather details to page
+    cloudOutput.innerHTML = data.current.cloud + "%";
+    HumidityOutput.innerHTML = data.current.humidity + "%";
+    windOutput.innerHTML = data.current.wind_kph + "km/h";
+
+    // set default time of day
+    let timeOfDay = "day";
+
+    // get unique id for wheather condition
+    const code = data.current.condition.code;
+
+    // change theme to night if it's night
+    if (!data.current.is_day) {
+      timeOfDay = "night";
     }
-  ).fail(function() {
-    console.log("An error occurred while making the request.");
-  });
+
+    if (code == 1000) {
+      // set back image to clear
+      app.style.backgroundImage = `url(./assets/img/${timeOfDay}/clear.jpg)`;
+
+      //change button back color
+      btn.style.background = "#e5ba92";
+      if (timeOfDay == "night") {
+        btn.style.background = "#181e27";
+      }
+    }
+    // set back image to cloudy
+    else if (
+      code == 1003 ||
+      code == 1006 ||
+      code == 1009 ||
+      code == 1030 ||
+      code == 1069 ||
+      code == 1087 ||
+      code == 1135 ||
+      code == 1273 ||
+      code == 1276 ||
+      code == 1279 ||
+      code == 1282
+    ) {
+      app.style.backgroundImage = `url(./assets/img/${timeOfDay}/cloudy.jpg)`;
+      btn.style.background = "#fa6d1b";
+      if (timeOfDay == "night") {
+        btn.style.background = "#181e27";
+      }
+    }
+    // set back image to rainy
+    else if (
+      code == 1063 ||
+      code == 1069 ||
+      code == 1072 ||
+      code == 1150 ||
+      code == 1153 ||
+      code == 1180 ||
+      code == 1183 ||
+      code == 1186 ||
+      code == 1189 ||
+      code == 1192 ||
+      code == 1195 ||
+      code == 1204 ||
+      code == 1207 ||
+      code == 1240 ||
+      code == 1243 ||
+      code == 1246 ||
+      code == 1249 ||
+      code == 1252
+    ) {
+      app.style.backgroundImage = `url(./assets/img/${timeOfDay}/rainy.jpg)`;
+      btn.style.background = "#647d75";
+      if (timeOfDay == "night") {
+        btn.style.background = "#325c80";
+      }
+    }
+    // set back image to snowy
+    else {
+      app.style.backgroundImage = `url(./assets/img/${timeOfDay}/snowy.jpg)`;
+      btn.style.background = "#4d72aa";
+      if (timeOfDay == "night") {
+        btn.style.background = "#1b1b1b";
+      }
+    }
+
+    app.style.opacity = "1";
+  }
+
+  function fetchData() {
+    $.post("./backend.php", { city: cityname }, function (data) {
+      console.log(data);
+    }).done((data) => {
+      try {
+        handleResponse(data);
+      } catch (error) {
+        alert(data);
+      }
+      loop = false;
+
+    });
+  }
+
+  fetchData();
+
+  // use setInterval to periodically fetch the data until it is successful
+  const intervalId = setInterval(function () {
+    if (!loop) {
+      clearInterval(intervalId);
+    } else {
+      fetchData();
+    }
+  }, 1000);
 }
 
 //Add click event to each city in the panel
@@ -38,8 +165,16 @@ for (let i = 0; i < cities.length; i++) {
   cities[i].addEventListener("click", (e) => {
     cityInput = e.target.innerHTML;
 
+    // clear previous inteval
+    try {
+      clearInterval(intervalData);
+    } catch (error) {}
+
     // Get data from API
-    fetchweatherdata(cityInput);
+    fetchWeatherData(cityInput);
+    const intervalData = setInterval(() => {
+      fetchWeatherData(cityInput);
+    }, 60000);
 
     app.style.opacity = "0";
   });
@@ -53,8 +188,13 @@ form.addEventListener("submit", (e) => {
   } else {
     cityInput = search.value;
 
+    // clear previous inteval
+    try {
+      clearInterval(intervalData);
+    } catch (error) {}
+
     // Get data from API
-    fetchweatherdata(cityInput);
+    fetchWeatherData(cityInput);
 
     search.value = "";
     app.style.opacity = "0";
@@ -73,5 +213,12 @@ function dayOfTheWeek(day, month, year) {
     "Saturday",
   ];
 
-  return weekday[new Date("${day}/${month}/${year}").getDay()];
+  return weekday[new Date(`${day}/${month}/${year}`).getDay()];
 }
+
+// update data every 60 sec
+const intervalData = setInterval(() => {
+  fetchWeatherData(cityInput);
+}, 60000);
+
+app.style.opacity = "1";
